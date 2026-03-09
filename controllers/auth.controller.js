0b1fe0606bce7c6535a2628a6f8e3ede827bbfa9
Neeaj Kumar <neerajkumar08697@gmail.com>
2026-03-09 12:34:32 +0530

    feat: implement jwt token generation for authentication

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { JWT_EXPIRED_IN, JWT_SECRET } from "../config/env.js";
 
 //user signup controller
 export const signUp = async (req, res, next) => {
@@ -14,18 +16,27 @@ export const signUp = async (req, res, next) => {
       const error = new Error("User already exists");
       res.status(409);
       throw error;
-    } 
+    }
 
     //has password
     const generateSalt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, generateSalt);
 
-    const newUser = await User.create({ name, email, hashedPassword });
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    //generate token
+    const token = jwt.sign({ userId: newUser?._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRED_IN,
+    });
 
     return res.status(201).json({
       success: true,
       message: "User created successfully",
-      data: newUser,
+      data: { token, user: newUser },
     });
   } catch (error) {
     console.error(error);
