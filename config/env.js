39ecd5db0690f2e377dbc2b086367deba4fd7442
Neeaj Kumar <neerajkumar08697@gmail.com>
2026-03-11 15:52:37 +0530

    chore:export environment variables from env.js

--- config/env.js
+++ config/env.js
@@ -5,4 +5,5 @@ config({
   path: `.env.${process.env.NODE_ENV || "development"}.local`,
 });
 
-export const { PORT, DB_URL, NODE_ENV } = process.env;
+export const { PORT, DB_URL, NODE_ENV, JWT_SECRET, JWT_EXPIRED_IN } =
+  process.env;
