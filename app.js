765bc00cb21087d76c2625a4d3a140271a031e4a
Neeaj Kumar <neerajkumar08697@gmail.com>
2026-03-11 15:55:40 +0530

    feat:add job route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectToDatabase from "./database/db.js";
 import authRouter from "./routes/auth.routes.js";
+import jobRouter from "./routes/job.routes.js";
 const app = express();
 
 connectToDatabase();
@@ -9,5 +10,6 @@ app.use(express.json());
 
 //routes
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobRouter);
 
 export default app;
